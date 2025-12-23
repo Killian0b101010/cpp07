0d9e2f08c92ec97b00fb0279d7b92ec05278a27c
Killian0b101010 <teixeira.killian44600@gmail.com>
2025-12-24 00:20:12 +0100

    peur de tous cassser

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -14,16 +14,84 @@
 #define ARRAY_HPP
 #include <iostream>
 #include <string>
-#include <array>
+#include <exception>
+
+template <typename T>
 class Array
 {
   private : 
-  T *array; 
+  
+  T *_array;
+  size_t _arrLength;
+  
   public:
   Array();
   ~Array();
   Array(unsigned int n);
-   &operator=(const Array &cpy);
-  Array &operator[](const Array &cpy);
+  Array(const Array &cpy);
+  Array &operator=(const Array &cpy);
+  T &operator[](size_t index);
+  T const &operator[](size_t index)const ;
+  size_t size()const;
 };
+
+template<typename T> 
+Array <T>::Array() : _array(NULL), _arrLength(0)
+{
+    
+}
+
+template<typename T> 
+Array<T>::~Array()
+{
+    delete _array;
+}
+
+template<typename T> 
+Array<T>::Array(unsigned int n)
+{
+    _array = new T[n]();
+    _arrLength = n;
+}
+
+template <typename T> 
+Array<T>::Array(const Array &cpy)
+{
+    _array._arrLength = cpy._arrLength;
+    _array = new T[_arrLength];
+    for(size_t i = 0; i < cpy._arrLength;i++)
+        _array[i] = cpy._array[i];
+}
+
+template <typename T> 
+Array<T> &Array <T>::operator=(const Array &cpy)
+{
+    if(this != cpy)
+    {
+        delete _array; 
+        _array = new T[_arrLength];
+        cpy._array = new T[_arrLength];
+        for(int i = 0; i < cpy._arrLength;i++)
+            _array[i] = cpy._array[i];
+    }
+    return(*this);
+}
+template <typename T> 
+T &Array<T>::operator[](size_t index)
+{
+    if(index >= _arrLength || index < 0)
+        throw(std::out_of_range("index incorrect"));
+    return(_array[index]);
+}
+
+template <typename T> 
+T const &Array<T>::operator[](size_t index)const
+{
+     if(index >= _arrLength || index < 0)
+        throw(std::out_of_range("index incorrect"));
+    return(_array[index]);
+}
+
+template <typename T> 
+size_t size(){return(_arrLength);}
 #endif
--- ex02/Array.tpp
+++ ex02/Array.tpp
@@ -10,12 +10,70 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-template <typename T,typename B>
+#ifndef ARRAY_TPP
+#define ARRAY_TPP
+#include <cstddef>
+#include <iostream>
+
+template<typename T> 
+Array <T>::Array() : _array(NULL), _arrLength(0)
 {
-    void createTab(T *array,size_t len)
-    {
-        
-        for(array)
-    }
     
-};
+}
+
+template<typename T> 
+Array<T>::~Array()
+{
+    delete _array;
+}
+
+template<typename T> 
+Array<T>::Array(unsigned int n)
+{
+    _array = new T[n]();
+    _arrLength = n;
+}
+
+template <typename T> 
+Array<T>::Array(const Array &cpy)
+{
+    _array._arrLength = cpy._arrLength;
+    _array = new T[_arrLength];
+    for(size_t i = 0; i < cpy._arrLength;i++)
+        _array[i] = cpy._array[i];
+}
+
+template <typename T> 
+Array<T> &Array <T>::operator=(const Array &cpy)
+{
+    if(this != cpy)
+    {
+        delete _array; 
+        _array = new T[_arrLength];
+        cpy._array = new T[_arrLength];
+        for(int i = 0; i < cpy._arrLength;i++)
+            _array[i] = cpy._array[i];
+    }
+    return(*this);
+}
+
+template <typename T> 
+T &Array<T>::operator[](size_t index)
+{
+    if(index >= _arrLength || index < 0)
+        throw(std::out_of_range("index incorrect"));
+    return(_array[index]);
+}
+
+template <typename T> 
+T const &Array<T>::operator[](size_t index)const
+{
+     if(index >= _arrLength || index < 0)
+        throw(std::out_of_range("index incorrect"));
+    return(_array[index]);
+}
+
+template <typename T> 
+Array::size_t size(){return(_arrLength);}
+
+#endif 
